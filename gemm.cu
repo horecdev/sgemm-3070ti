086161970fc5d3e7b4a66c3e407edbf3bef4196c
Ozehownik <horecki.michal@gmail.com>
2026-09-22 18:54:36 +0200

    Final code + benchmark

--- gemm.cu
+++ gemm.cu
@@ -5,6 +5,7 @@
 #include <cublas_v2.h>
 #include <iostream>
 #include <stdexcept>
+#include <cstdlib>
 
 __global__ void sgemm_naive(int M, int K, int N, float* p_A, float* p_B, float* p_C) {
     int row = blockDim.y * blockIdx.y + threadIdx.y;
@@ -21,10 +22,75 @@ __global__ void sgemm_naive(int M, int K, int N, float* p_A, float* p_B, float* 
     }
 }
 
+#define TILE_SIZE 16 // must be known at compile time 
+// launches one thread for every output elem in C just like naive
+
+// logically explaining. To calculate a 16x16 patch of C, the block needs a horizontal stripe of A (16 rows tall, 1024 cols wide)
+// and a vertical stripe of B (1024 rows tall, 16 cols wide)
+
+// the big ass 16x1024 or 1024x16 is chopped into 16x16 tiles that are sliding across K (since to calculate elems you go over whole K)
+__global__ void sgemm_tiled(int M, int K, int N, float* p_A, float* p_B, float* p_C) {
+    __shared__ float tile_A[TILE_SIZE][TILE_SIZE];
+    __shared__ float tile_B[TILE_SIZE][TILE_SIZE];
+
+    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    int col = blockIdx.x * TILE_SIZE + threadIdx.x;
+
+    float sum = 0.0f; // one for each thread
+
+    int total_phases = (K + TILE_SIZE - 1) / TILE_SIZE; // round it up
+    for (int phase = 0; phase < total_phases; ++phase) {
+        if (row < M && (phase * TILE_SIZE + threadIdx.x) < K) { // didnt go off bounds and viable to slide
+            tile_A[threadIdx.y][threadIdx.x] = p_A[row * K + (phase * TILE_SIZE + threadIdx.x)];
+        }
+        else {
+            tile_A[threadIdx.y][threadIdx.x] = 0.0f; // got off bounds (tile OR row)
+            // you can set to 0.0f because later you multiply with respective piece from B (also went off bounds, cuz its both K) and 0 * 0 = 0 in sum accum. 
+            // There is no difference.
+        }
+
+        if ((phase * TILE_SIZE + threadIdx.y) < K && col < N) { // same bounds (tile OR col)
+            tile_B[threadIdx.y][threadIdx.x] = p_B[(phase * TILE_SIZE + threadIdx.y) * N + col];
+        }
+        else {
+            tile_B[threadIdx.y][threadIdx.x] = 0.0f;
+        }
+
+        __syncthreads(); // so its all filled up
+
+        // now you REUSE numbers that OTHER THREADS loaded up to tiles.
+        // load from A across K horizontal, from B across K vertical
+        for (int i = 0; i < TILE_SIZE; ++i) {
+            sum += tile_A[threadIdx.y][i] * tile_B[i][threadIdx.x];
+        }
+
+        // must sync bc some threads may finish faster and overwrite shared memory
+        __syncthreads();
+    }
+
+    if (row < M && col < N) {
+        p_C[row * N + col] = sum;
+    }
+}
+
+// say this is the part of C you are calculating (left) C = AB
+// then the tiles created are on the right
+//       C                      A                       B
+// [             ]       [               ]       [       [  1  ]  ]
+// [             ]       [               ]       [       [     ]  ]
+// [      [   ]  ]       [[ 1 ][ 2 ][ 3 ]]       [       [  2  ]  ]
+// [      [   ]  ]       [[   ][   ][   ]]       [       [     ]  ]
+// [             ]       [               ]       [       [  3  ]  ]
+// [             ]       [               ]       [       [     ]  ]
+// this is so brilliant, because one thread used to load K times from A and K times from B.
+// now it loads once per tile for A and B. Thats K / TILE_SIZE reads from VRAM. TILE_SIZE TIMES LESS.
+
+
 void print_upper_left_square(float* p_matrix, int M, int N, int side_length) {
     if (side_length >= M || side_length >= N) {
         throw std::runtime_error("side_length is too big");
     }
+    std::cout << "Upper left " << side_length << "x" << side_length << " square:" << std::endl;
     for (int i = 0; i < side_length; ++i) {
         std::cout << "[";
         for (int j = 0; j < side_length; ++j) {
@@ -35,12 +101,22 @@ void print_upper_left_square(float* p_matrix, int M, int N, int side_length) {
         }
         std::cout <<"]" << std::endl;
     }
-
 }
+
+float max_difference(float* p_m1, float* p_m2, int num_elems) {
+    float max_diff = 0;
+
+    for (int i = 0; i < num_elems; ++i) {
+        max_diff = std::max(max_diff, std::abs(p_m1[i] - p_m2[i]));
+    }
+
+    return max_diff;
+}
+
 int main() {
-    int M = 1024;
+    int M = 2048;
     int N = 1024;
-    int K = 1024;
+    int K = 8192;
 
     size_t bytes_A = M * K * sizeof(float);
     size_t bytes_B = K * N * sizeof(float);
@@ -48,13 +124,15 @@ int main() {
 
     float* host_A = static_cast<float*>(malloc(bytes_A));
     float* host_B = static_cast<float*>(malloc(bytes_B));
-    float* host_C = static_cast<float*>(malloc(bytes_C));
+    float* host_C_naive = static_cast<float*>(malloc(bytes_C));
+    float* host_C_tiled = static_cast<float*>(malloc(bytes_C));
+    float* host_C_cublas = static_cast<float*>(malloc(bytes_C));
 
     for (int i = 0; i < M * K; ++i) {
-        host_A[i] = 1.0f;
+        host_A[i] = static_cast<float>(rand() % 7 - 3);
     }
     for (int i = 0; i < K * N; ++i) {
-        host_B[i] = 1.0f;
+        host_B[i] = static_cast<float>(rand() % 7 - 3);
     }
 
     float* gpu_A;
@@ -67,6 +145,11 @@ int main() {
     cudaMemcpy(gpu_A, host_A, bytes_A, cudaMemcpyHostToDevice);
     cudaMemcpy(gpu_B, host_B, bytes_B, cudaMemcpyHostToDevice);
 
+    dim3 threads_per_block(16, 16);
+    // x is horizontal, y is vertical. thats why M, N maps to Y, 
+    // also we launch one block for one element in the result matrix, so row is M, col is N
+    dim3 num_blocks((N + threads_per_block.x - 1) / threads_per_block.x, (M + threads_per_block.y - 1) / threads_per_block.y);
+
     // prep for benchmarking
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
@@ -78,11 +161,6 @@ int main() {
 
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!
     // BENCHMARK THE NAIVE VERSION
-
-    dim3 threads_per_block(16, 16);
-    // x is horizontal, y is vertical. thats why M, N maps to Y, 
-    // also we launch one block for one element in the result matrix, so row is M, col is N
-    dim3 num_blocks((N + threads_per_block.x - 1) / threads_per_block.x, (M + threads_per_block.y - 1) / threads_per_block.y);
 
     for (int i = 0; i < warmup; ++i) {
         sgemm_naive<<<num_blocks, threads_per_block>>>(M, K, N, gpu_A, gpu_B, gpu_C);
@@ -100,11 +178,28 @@ int main() {
 
     std::cout << "Naive GEMM: " << (milliseconds / runs) << "ms" << std::endl;
 
-    cudaMemcpy(host_C, gpu_C, bytes_C, cudaMemcpyDeviceToHost);
-    print_upper_left_square(host_C, M, N, 5);
+    cudaMemcpy(host_C_naive, gpu_C, bytes_C, cudaMemcpyDeviceToHost);
 
     // !!!!!!!!!!!!!!!!!!!!!!
     // BENCHMARK THE TILED KERNEL
+
+    for (int i = 0; i < warmup; ++i) {
+        sgemm_tiled<<<num_blocks, threads_per_block>>>(M, K, N, gpu_A, gpu_B, gpu_C);
+    }
+    cudaDeviceSynchronize();
+    std::cout << "CUDA Error: " << cudaGetErrorString(cudaGetLastError()) << std::endl;
+
+    cudaEventRecord(start);
+    for (int i = 0; i < runs; ++i) {
+        sgemm_tiled<<<num_blocks, threads_per_block>>>(M, K, N, gpu_A, gpu_B, gpu_C);
+    }
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    std::cout << "Tiled GEMM: " << (milliseconds / runs) << "ms" << std::endl;
+
+    cudaMemcpy(host_C_tiled, gpu_C, bytes_C, cudaMemcpyDeviceToHost);
 
     // !!!!!!!!!!!!!!!!
     // BENCHMARK CUBLAS
@@ -148,13 +243,14 @@ int main() {
 
     std::cout << "cuBLAS GEMM: " << (milliseconds / runs) << "ms" << std::endl;
     
-    cudaMemcpy(host_C, gpu_C, bytes_C, cudaMemcpyDeviceToHost);
-    print_upper_left_square(host_C, M, N, 5);
+    cudaMemcpy(host_C_cublas, gpu_C, bytes_C, cudaMemcpyDeviceToHost);
 
-    std::cout << "Finished" << std::endl;
+    std::cout << "Max differences: " << std::endl;
+    std::cout << "Naive vs cuBLAS: " << max_difference(host_C_naive, host_C_cublas, M * N) << std::endl;
+    std::cout << "Tiled vs cuBLAS: " << max_difference(host_C_tiled, host_C_cublas, M * N) << std::endl;
 
     // FREE MEMORY
 
     cudaFree(gpu_A); cudaFree(gpu_B); cudaFree(gpu_C);
-    free(host_A); free(host_B); free(host_C);
+    free(host_A); free(host_B); free(host_C_naive); free(host_C_tiled); free(host_C_cublas);
 }
